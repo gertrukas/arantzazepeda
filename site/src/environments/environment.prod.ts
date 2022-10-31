e4d86d8ce4422102c34778b70c0bdf762317279b
gerardo <trujillo@excess.com.mx>
2022-10-31 13:31:41 -0600

    Enviroments PROD

--- site/src/environments/environment.prod.ts
+++ site/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   appVersion: 'v1.0.1',
   USERDATA_KEY: 'authexcess2022',
-  apiUrl: 'https://backend.valterkuhne.com/api',
-  adminUrl: 'https://admin.valterkuhne.com/',
-  pageUrl: 'https://valterkuhne.com/',
+  apiUrl: 'https://backend.arantzazepeda.com/api',
+  adminUrl: 'https://admin.arantzazepeda.com/',
+  pageUrl: 'https://arantzazepeda.com/',
 };
